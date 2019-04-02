4628e6c08b80878250723f0e864b801fa028a6e9
mrc <mrc.moresi@gmail.com>
2019-04-01 23:17:37 -0300

    add backoff implementation, working but not passing all the tests Ran 11, 4 failed. WIP

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -5,7 +5,8 @@ import itertools
 
 
 def log2ext(x):
-    """
+    """Extend log function.
+
     if x = 0 ==> -inf
     if x != 0 ==> log2(x)
     """
@@ -156,7 +157,8 @@ class AddOneNGram(NGram):
     """Add-one estimation (Laplace smoothing)."""
 
     def __init__(self, n, sents):
-        """
+        """Init.
+
         n -- order of the model.
         sents -- list of sentences, each one being a list of tokens.
         """
@@ -164,9 +166,10 @@ class AddOneNGram(NGram):
         super().__init__(n, sents)
 
         # compute vocabulary
-        # chain.from_iterable equivale a for sent in sents: for elem in sent
-        # despliego todos los sents y armo un conjunto agrengando el toke de cierre
-        self._voc = voc = set(list(itertools.chain.from_iterable(sents)) + ['</s>'])
+        # chain.from_iterable is the same for sent in sents: for elem in sent
+        # expand all the sent and build a set
+        self._voc = voc = set(
+            list(itertools.chain.from_iterable(sents)) + ['</s>'])
         # WORK HERE!!
         self._V = len(voc)  # vocabulary size
 
@@ -203,9 +206,11 @@ class AddOneNGram(NGram):
 
 
 class InterpolatedNGram(NGram):
+    """Intepolate model."""
 
     def __init__(self, n, sents, gamma=None, addone=True):
-        """
+        """Init.
+
         n -- order of the model.
         sents -- list of sentences, each one being a list of tokens.
         gamma -- interpolation hyper-parameter (if not given, estimate using
@@ -266,7 +271,6 @@ class InterpolatedNGram(NGram):
             print("Choosed gamma {}".format(min_gamma))
             print("With perplexity {}".format(min_p))
 
-
     def count(self, tokens):
         """Count for an k-gram for k <= n.
 
@@ -285,17 +289,16 @@ class InterpolatedNGram(NGram):
         n = self._n
         prob = 0
 
-        
         if prev_tokens is None:
             prev_tokens = ()
-        
+
         # join tokens
         ngram = prev_tokens + (token,)
-        
+
         # constraint
         # sum(lambdas) = 1 with lambda_i >= 0
         lambdas_factor = 1.
-        
+
         # calculate q_ML from n-gram down to unigram
         for i in range(n):
             # q_ML(token | prev_tokens) = count_token / count_prev_tokens.
@@ -305,24 +308,25 @@ class InterpolatedNGram(NGram):
             # ngram
             i_gram = ngram[i:]
             i_count = self.count(i_gram)
-            
+
             # ngram-1 with one token less
-            i_less_one_gram = i_gram[:-1]             
+            i_less_one_gram = i_gram[:-1]
             i_less_one_count = self.count(i_less_one_gram)
-            
+
             # For every i-gram with 1 < i
             if 1 < len(i_gram):
                 prob += lambdas_factor * i_count / (i_less_one_count + gamma)
-                curr_lamb = lambdas_factor * i_less_one_count / (i_less_one_count + gamma)
+                curr_lamb = (
+                    lambdas_factor * i_less_one_count / (i_less_one_count + gamma))
                 lambdas_factor -= curr_lamb
-            # For 1-grams with addone 
+            # For 1-grams with addone
             elif len(i_gram) == 1 and self._addone:
                 # c(i_gram) +1 / c(i_gram_-1) + V
                 prob += lambdas_factor * (i_count + 1) / (i_less_one_count + self._V)
             # For 1-grams w/o addone
             else:
                 prob += lambdas_factor * i_count / i_less_one_count
-        
+
         return prob
 
 
@@ -339,15 +343,15 @@ class BackOffNGram(LanguageModel):
             held-out data).
         addone -- whether to use addone smoothing (default: True).
         """
-        super().__init__(n, sents)
+        # super().__init__(n, sents)
         self.beta = beta
         self.addone = addone
-        self.counts = counts = defaultdict(int)
         self.set_A = set_A = defaultdict(set)
         self.voc = voc = set(
             list(itertools.chain.from_iterable(sents)) + ['</s>'])
         self.V = len(voc)
         self.models = models = []
+        self.n = n
 
         if beta is None:
             # 90% training, 10% held-out
@@ -357,56 +361,72 @@ class BackOffNGram(LanguageModel):
         else:
             train_sents = sents
 
-        #  ngrams models n = 1, 2, 3, ... n
+        # unigrams
+        if self.addone:
+            models.append(AddOneNGram(1, train_sents))
+        else:
+            models.append(NGram(1, train_sents))
+        #  ngrams models n = 2, 3, ... n
         # and put it in a list [1-gram, 2-gram, 3-gram, ... ,n-gram]
-        for i in range(1, n + 1):
-            models.append(Ngram(i, train_sents))
+        for i in range(2, n + 1):
+            models.append(NGram(i, train_sents))
 
-        for model in models[:1]:
-            n_of_model = model.n
-            for ngram, val in model.count.items():
+        for model in models[1:]:
+            n_of_model = model._n
+            for ngram, val in model._count.items():
+
+                # print("ngram {} val {}".format(ngram, val))
                 if len(ngram) == n_of_model:
                     n_m1_gram = ngram[:-1]
-                    # build A set 
-                    set_a[n_m1_gram].add(ngram[-1])
+                    # print(n_m1_gram)
+
+                    # build A set
+                    set_A[n_m1_gram].add(ngram[-1])
+                    # print(set_A)
 
         if beta is None:
             m = int(0.9 * len(sents))
             held_out_sents = sents[m:]
             beta = self.calculate_beta(held_out_sents)
         else:
-            self.alpha = self.calculate_alpha()
-            self.denom = self.calculate_denominator()
+            self._alpha = self.calculate_alpha()
+            self._denom = self.calculate_denominator()
 
-    def A(self, tokens):
-        """Set of words with counts > 0 for a k-gram with 0 < k < n.
-
-        tokens -- the k-gram tuple.
+    def calculate_alpha(self):
         """
-        return self._A[tokens]
+        Calculate alpha.
 
-    def alpha(self, tokens):
-        """Missing probability mass for a k-gram with 0 < k < n.
-
-        tokens -- the k-gram tuple.
+        alpha(x1, ... xi )= 1 - sum(c_discount(x1 .. xi x) / c(x1 .. xi))
         """
-        return self.alpha.get(tokens, 1.)
+        acc = 0
+        _alpha = defaultdict(float)
+        for ngram, amount in self.set_A.items():
+            # print(self.set_A.items())
+            for x in amount:
+                c_discount = self.count(ngram + tuple([x])) - self.beta
+                c = self.count(ngram)
+                acc += c_discount / c
+            _alpha[ngram] = 1. - acc
 
-    def denom(self, tokens):
-        """Normalization factor for a k-gram with 0 < k < n.
+        return _alpha
 
-        tokens -- the k-gram tuple.
+    def calculate_denominator(self):
+        """Calculate denominator.
+
+        denom(x1, .. xi) = 1 - sum(qd(x|x2 .. xi-1))
         """
-        return self.denom.get(tokens, 1.)
-
-    def count(self, tokens):
-        """Return length of the token."""
-        n = len(tokens)
-        # check if it is first token
-        if tokens == n * ('<s>',):
-            n += 1
-        count = self.models[n - 1].counts[tokens]
-        return count
+        acc = 0.
+        _denom = defaultdict(float)
+        for ngram, amount in self.set_A.items():
+            # print('Adentro del for')
+            #print(ngram, amount)
+            for x in amount:
+                #print(amount)
+                #print(x)
+                # print(x, ngram[1:])
+                acc += self.cond_prob(x, ngram[1:])
+            _denom[ngram] = 1. - acc
+        return _denom
 
     def calculate_beta(self, held_out_sents):
         """Estimate beta param using held-out data."""
@@ -417,12 +437,12 @@ class BackOffNGram(LanguageModel):
         for i in [float(x * 0.05) for x in range(21)]:
             self.beta = i
             # need calculate alpha and denominator to calculare log_prob
-            self.alpha = self.calculate_alpha()
-            self.denom = self.calculate_denominator()
+            self._alpha = self.calculate_alpha()
+            self._denom = self.calculate_denominator()
             lp = self.log_prob(held_out_sents)
             print(
                 "Beta {} Alpha {} denom {} log prob {}".format(
-                    self.beta, self.alpha, self.denom, lp))
+                    self.beta, self._alpha, self.denom, lp))
             if max_bound < lp:
                 max_bound = lp
                 temp = i
@@ -430,5 +450,80 @@ class BackOffNGram(LanguageModel):
         # beta between 0 and 1
         assert(temp >= 0 and temp <= 1)
         self.beta = temp
-        self.alpha = self.calculate_alpha()
-        self.denom = self.calculate_denominator()
+        # recalculate alpha and denominator after new beta
+        self._alpha = self.calculate_alpha()
+        self._denom = self.calculate_denominator()
+
+    def A(self, tokens):
+        """Set of words with counts > 0 for a k-gram with 0 < k < n.
+
+        tokens -- the k-gram tuple.
+        """
+        return self.set_A.get(tokens, set())
+
+    def alpha(self, tokens):
+        """Missing probability mass for a k-gram with 0 < k < n.
+
+        tokens -- the k-gram tuple.
+        """
+        return self._alpha.get(tokens, 1.)
+
+    def denom(self, tokens):
+        """Normalization factor for a k-gram with 0 < k < n.
+
+        tokens -- the k-gram tuple.
+        """
+        return self._denom.get(tokens, 1.)
+
+    def count(self, tokens):
+        """Return length of the token."""
+        n = len(tokens)
+        # check if it is first token
+        if tokens == n * ('<s>',):
+            n += 1
+        # print("COUUUUNNTT")
+        # print(tokens)
+        # print(n)
+        # print(self.models)
+        # print(self.models[n - 1]._count)
+        count = self.models[n - 1].count(tuple(tokens,))
+        return count
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        prob = 0.
+
+        # 1 gram model
+        if not prev_tokens:
+            #print("PREV TOKENS is NONE")
+            count = self.count(tuple([token]))
+            count_total = self.count(tuple())
+
+            if self.addone:
+                prob = (count + 1) / (count_total + self.V)
+            else:
+                prob = count / count_total
+        # all the others n-gram model
+        else:  
+            # check if xi belongs A(x1 , ... , xi-1)
+            if token in self.A(tuple(prev_tokens)):
+                tokens = tuple(prev_tokens) + tuple([token])
+                # c*(x1, ..., xi)
+                discounted = self.count(tokens) - self.beta
+                # c(x1, ..., xi-1)
+                count = self.count(tuple(prev_tokens))
+                prob = discounted / float(count)
+            else:
+                # alpha(x1, ..., xi-1)
+                alpha = self.alpha(tuple(prev_tokens))
+                # qd(xi|x2 ... xi-1)
+                prob_previous = self.cond_prob(token, prev_tokens[1:])
+                # denom(x1, ..., xi-1)
+                denominator = self.denom(tuple(prev_tokens))
+                if prob_previous != 0 and denominator != 0:
+                    prob = alpha * (prob_previous / denominator)
+        return prob
